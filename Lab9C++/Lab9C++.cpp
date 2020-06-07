c2894480ad1efe8b1d2025ab95315540dd8cbeaa
UnnamedH <55529629+UnnamedH@users.noreply.github.com>
2020-06-07 10:06:43 +0300

    debug

--- Lab9C++/Lab9C++.cpp
+++ Lab9C++/Lab9C++.cpp
@@ -3,17 +3,18 @@
 #include <windows.h>
 #include <conio.h>
 #include <vector>
+
 using namespace std;
 
-bool isTimeCorrect(tm proceduresTime[],int i) {
+bool isTimeCorrect(tm time) {
     bool value = false;
 
-    time_t year = proceduresTime[i].tm_year;
-    time_t month = proceduresTime[i].tm_mon;
-    time_t day = proceduresTime[i].tm_mday;
-    time_t hours = proceduresTime[i].tm_hour;
-    time_t minutes = proceduresTime[i].tm_min;
-    time_t seconds = proceduresTime[i].tm_sec;
+    time_t year = time.tm_year;
+    time_t month = time.tm_mon;
+    time_t day = time.tm_mday;
+    time_t hours = time.tm_hour;
+    time_t minutes = time.tm_min;
+    time_t seconds = time.tm_sec;
 
 
     if (month > 0 && month <= 12) {
@@ -60,58 +61,96 @@ bool isTimeCorrect(tm proceduresTime[],int i) {
     
 }
 
-void easy() {
-    time_t year;
-    time_t month;
-    time_t day;
-    time_t hours;
-    time_t minutes;
-    time_t seconds;
-    int colvoProcedures = 0;
-    char point;
-    char TwoPoins;
-    tm proceduresTime[100] = {};
-    int k = 0;
-    cout << "Нажмите E, если хотите завершить ввод." << endl;
-    while (!GetKeyState(VK_DELETE)) {
-        cout << "Введите время процедуры (dd:mm:yyyy hh:mm:ss): ";
-        while (cin >> day >> point >> month >> point >> year >> point >> hours >> TwoPoins >> minutes >> TwoPoins >> seconds) {
-            //struct tm newtime;
-            //time_t now = time(0);
-            //localtime_s(&newtime, &now);
-            proceduresTime[k].tm_year = year;
-            proceduresTime[k].tm_mon = month;
-            proceduresTime[k].tm_mday = day;
-            proceduresTime[k].tm_hour = hours;
-            proceduresTime[k].tm_min = minutes;
-            proceduresTime[k].tm_sec = seconds;
-            /*newtime.tm_year = year;
-            newtime.tm_mon = month;
-            newtime.tm_mday = day;
-            newtime.tm_hour = hours;
-            newtime.tm_min = minutes;
-            newtime.tm_sec = seconds;*/
-            
-            if (isTimeCorrect(proceduresTime,k) && point == '.' && TwoPoins == ':') {
-                colvoProcedures++;
-                break;
-            }
-        }
-        k++;
-    }
-    
-    for (int i = 1; i < colvoProcedures; i++) {
-        cout << endl <<"Время " << i++ << "процедуры" << proceduresTime[k].tm_mday << point << proceduresTime[k].tm_mon << 
-            point << proceduresTime[k].tm_year << point << proceduresTime[k].tm_hour 
-            << TwoPoins << proceduresTime[k].tm_min << TwoPoins << proceduresTime[k].tm_sec;
+time_t getTimeFromConsole() {
+    string date_str;
+    string time_str;
+    cin >> date_str >> time_str;
+
+    int day = 0;
+    int month = 0;
+    int year = 0;
+    if (sscanf_s(date_str.c_str(), "%d.%d.%d", &day, &month, &year) == -1) {
+        return -1;
     }
 
+    int hour = 0;
+    int min = 0;
+    int sec = 0;
+    if (sscanf_s(time_str.c_str(), "%d:%d:%d", &hour, &min, &sec) == -1) {
+        return -1;
+    }
+    
+    struct tm time = { 0 };
+
+    time.tm_year = year;
+    time.tm_mon = month;
+    time.tm_mday = day;
+    time.tm_hour = hour;
+    time.tm_min = min;
+    time.tm_sec = sec;
+
+    if (!isTimeCorrect(time)) {
+        return -1;
+    }
+
+    time.tm_year = time.tm_year - 1900;
+    time.tm_mon = time.tm_mon - 1;
+
+    return mktime(&time);
+}
+
+string formatTime(time_t timeSeconds)
+{
+    tm time;
+    localtime_s(&time, &timeSeconds);
+    char buffer[100];
+    strftime(buffer, 100, "%d.%m.%Y %H:%M:%S", &time);
+    return buffer;
 }
 
 int main()
 {
     setlocale(LC_ALL,"");
-    easy();
+    
+    cout << "Введите время первой процедуры (dd.mm.yyyy hh:mm:ss): ";
+    time_t firstTime = getTimeFromConsole();
+    if (firstTime == -1) {
+        cout << "Указано неверное время первой процедуры";
+        return 0;
+    }
+    
+    cout << "Введите время второй процедуры (dd.mm.yyyy hh:mm:ss): ";
+    time_t secondTime = getTimeFromConsole();
+    if (secondTime == -1) {
+        cout << "Указано неверное время второй процедуры";
+        return 0;
+    }
+
+    cout << "Введите количество процедур: ";
+    int numberOfProcedures;
+    cin >> numberOfProcedures;
+    if (numberOfProcedures <= 0) {
+        cout << "Указано неверное количество процедур";
+        return 0;
+    }
+
+    double interval = difftime(secondTime, firstTime);
+    if (interval <= 0) {
+        cout << "Конечное время должно быть позже чем начальное";
+        return 0;
+    }
+
+    time_t time = firstTime-3600;
+    for (int i = 1; i <= numberOfProcedures; i++) {
+        cout << endl << "Время " << i << " процедуры: " << formatTime(time);
+        time = time + interval;
+    }
+    cout << endl;
+
+    return 0;
+
 }
+
+
 
 
